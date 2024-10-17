ebc2977edf5030d2ffc64cff2bd2ab7902d81fe3
naylaaaaa19 <naylachaesa19@gmail.com>
2024-10-17 21:02:31 +0700

    Add files via upload

---
+++ nomor4.java
@@ -0,0 +1,18 @@
+public class nomor4 {
+    
+    public static void main(String[] args) {
+        int[][] array = {
+            {-1,0,-1,0,-1},
+            {0,-1,0,-1,0},
+            {-1,0,-1,0,-1},
+            {0,-1,0,-1,0}
+        };
+        
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 5; j++){
+                System.out.print(array[i][j] + "");
+            }
+            System.out.println();
+        }
+    }
+}
